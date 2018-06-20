47789457e493df9612885d109993bcd9e32868b5
vladovoev <vladovoev@yahoo.co.uk>
2018-06-20 18:36:52 +0300

    added checkout saga

--- client/src/reducers/canCheckOut.js
+++ client/src/reducers/canCheckOut.js
@@ -2,7 +2,6 @@ import { createReducer } from './../utility';
 import { SET_CAN_CHECK_OUT } from '../actions'
 export const canCheckOut = createReducer(0, {
     [SET_CAN_CHECK_OUT](state, { value }) {
-        debugger;
         return value;
     }
 });
